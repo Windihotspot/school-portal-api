e89230a2e0825288d6258b979d4404e7fb1edaee
ifiok <boiwindihotspot@gmail.com>
2024-12-29 04:45:37 +0100

    signup routes

--- db.js
+++ db.js
@@ -3,9 +3,19 @@ const mysql = require('mysql2');
 // Create a MySQL connection
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root', 
-  password: 'opium934', 
+  user: 'root',
+  password: 'opium934',
   database: 'school',
+  port: 3308,
+});
+
+// Check if the connection is successful
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Successfully connected to the database');
 });
 
 // Make the connection promisified
